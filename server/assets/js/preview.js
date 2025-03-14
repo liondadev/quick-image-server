a47459ea1950106d04debb2c01774949f8890c30
Lion <59421259+liondadev@users.noreply.github.com>
2025-03-14 18:27:17 -0400

    more stuff

--- server/assets/js/preview.js
+++ server/assets/js/preview.js
@@ -13,6 +13,21 @@ let titleElement;
  */
 let previewContainer;
 
+/**
+ * @type {HTMLAnchorElement}
+ */
+let openUrlButton;
+
+/**
+ * @type {HTMLAnchorElement}
+ */
+let openThumbButton;
+
+/**
+ * @type {HTMLAnchorElement}
+ */
+let deleteButton;
+
 const toaster = new Notyf({dismissible: true, duration: 2000});
 
 /**
@@ -22,16 +37,25 @@ const toaster = new Notyf({dismissible: true, duration: 2000});
  * @param {string} ext
  * @param {string} mimeType
  * @param {number} uploadedAt
+ * @param {string} deleteToken
  * @return {boolean}
  */
-function showImagePreview(name, id, ext, mimeType, uploadedAt) {
+function showImagePreview(name, id, ext, mimeType, uploadedAt, deleteToken) {
     // Ensure all the required elements are created and exist.
     if (!popupElement) return false;
     if (!titleElement) return false;
+    if (!openUrlButton) return false;
+    if (!openThumbButton) return false;
+    if (!deleteButton) return false;
     const targetUrl = "/f/"+id+ext;
+    const deleteUrl = "/delete/"+id+"/"+deleteToken;
 
     titleElement.innerText = name;
     previewContainer.src = targetUrl;
+
+    openUrlButton.href = targetUrl;
+    deleteButton.href = deleteToken;
+    openThumbButton.href = "/thumb/"+id+".png"
 
     popupElement.showModal();
     return true;
@@ -41,6 +65,10 @@ window.onload = function() {
     popupElement = document.getElementById("upload-preview-modal");
     titleElement = document.getElementById("upload-preview-modal-title");
     previewContainer = document.getElementById("upload-preview-modal-preview-container");
-    const res = showImagePreview("Amazing Image!", "GFORxwmAsPQR", ".png", "image/png", 1741908775);
+    openUrlButton = document.getElementById("upload-preview-btn-open-url");
+    openThumbButton = document.getElementById("upload-preview-btn-open-thumb");
+    deleteButton = document.getElementById("upload-preview-btn-delete");
+
+    // const res = showImagePreview("Amazing Image!", "WfLFgTapmfUa", ".png", "image/png", 1741908775);
     console.log(res);
 }
